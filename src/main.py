f854c1c4df92a7c13eb87a4bf1aa3cc909275233
LVA720 <158532152+LVA720@users.noreply.github.com>
2025-10-23 11:18:49 +0900

    Layout e logo

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 from dash import Dash
-from layout import calculator_layout
+from layout.calculator_layout import Layout
 import dash_bootstrap_components as dbc
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-
-app.layout = calculator_layout.create_layout()
+app.layout = Layout.create_layout()
 
 if __name__ == "__main__":
     app.run(debug=True)
