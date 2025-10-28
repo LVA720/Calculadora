070c1910bb89bc5cdeb940e4336acd097ce98424
LVA720 <158532152+LVA720@users.noreply.github.com>
2025-10-28 10:20:10 +0900

    arrumando o callback

--- src/callbacks/calculator_callbacks.py
+++ src/callbacks/calculator_callbacks.py
@@ -10,18 +10,35 @@ from logic.calculator_logic import evaluate_expression
 def on_click(n_clicks, current_display):
     if not ctx.triggered_id:
         return current_display or "0"
-    button = ctx.triggered_id
-    if button == "C":
+    
+    button_value = str(ctx.triggered_id["index"])
+    operators = ["+", "-", "x", "÷", "%"]
+
+    if not current_display or current_display == "0":
+        if button_value in operators or button_value == ".":
+            return current_display
+        else:
+            return button_value
+        
+    if button_value == "C":
         return "0"
-    elif button == "CE":
+    
+    elif button_value == "CE":
         return current_display[:-1] if len(current_display) > 1 else "0"
-    elif button == "=":
+    
+    elif button_value == "=":
         try:
             return str(evaluate_expression(current_display))
         except Exception:
             return "Erro"
-    else:
-        if current_display == "0":
-            current_display = ""
-            return current_display + str(button)
+        
+    if current_display[-1] in operators and button_value in operators:
+        return current_display
     
+    if button_value == ".":
+        last_number = current_display.split("+")[-1].split("x")[-1].split("÷")[-1]
+        if "." in last_number:
+            return current_display
+    
+    new_display = current_display + button_value 
+    return new_display
