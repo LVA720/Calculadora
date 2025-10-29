b70aa7dbca00fd25f399c74c167b064da3589755
kazukito4 <kazukazukito@gmail.com>
2025-10-30 00:51:41 +0900

    ta bom

---
+++ src/layout/calculator_button.py
@@ -0,0 +1,39 @@
+from dash import html
+import dash_bootstrap_components as dbc
+
+def create_buttom():
+
+    layout = [
+        ["ON/C","CE","MRC","M-","M+"],
+        ["7","8","9","%","√"],
+        ["4","5","6","x","÷"],
+        ["1","2","3","+","-"],
+        ["0",".","+/-","="]
+    ]
+
+    buttons=[]
+    for row in layout:
+        for label in row:
+            color = "light" if label.isdigit() else "dark"
+            button = dbc.Button(
+                label,
+                color=color,
+                id={"type":"button","index":label},
+                className="me-1",
+                style={"color":"#058105"}
+            )
+            buttons.append(button)
+ 
+    return html.Div(
+        buttons,
+        style={
+            "display": "grid",
+            "gridTemplateColums":"repeat(5,1fr)",
+            "gap": "10px",
+            "width":"100%",
+            "margin":"auto",
+            "padding":"10px",
+        },
+    )
+    
+    
